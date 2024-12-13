2fa343e868386f7312d07e441999974850a453f6
wefashe <wefashe@qq.com>
2024-12-13 14:19:19 +0800

    游戏设置

--- src/config.js
+++ src/config.js
@@ -2,83 +2,28 @@ import React from "react";
 
 const config = {
   ROMS: {
+    1942: {
+      name: "1942",
+      url: "/roms/classic/1942.nes"
+    },
+    mario: {
+      name: "超级玛丽",
+      url: "https://lab.ur1.fun/FCGames/roms/classic/mario.nes"
+    },
     owlia: {
-      name: "The Legends of Owlia",
-      description: (
-        <span>
-          <a
-            href="http://www.gradualgames.com/p/the-legends-of-owlia_1.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Owlia by Gradual Games
-          </a>{" "}
-          /{" "}
-          <a
-            href="http://www.infiniteneslives.com/owlia.php"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Buy the cartridge
-          </a>
-        </span>
-      ),
+      name: "奥薇利亚传奇",
       url: "https://cdn.jsdelivr.net/gh/bfirsh/jsnes-roms@master/owlia.nes"
     },
     nomolos: {
       name: "Nomolos: Storming the Catsle",
-      description: (
-        <span>
-          <a
-            href="http://www.gradualgames.com/p/nomolos-storming-catsle.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Monolos by Gradual Games
-          </a>{" "}
-          /{" "}
-          <a
-            href="http://www.infiniteneslives.com/nomolos.php"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Buy the cartridge
-          </a>
-        </span>
-      ),
       url: "https://cdn.jsdelivr.net/gh/bfirsh/jsnes-roms@master/nomolos.nes"
     },
     croom: {
       name: "Concentration Room",
-      description: (
-        <span>
-          <a
-            href="http://www.pineight.com/croom/README"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Concentration Room
-          </a>{" "}
-          by Damian Yerrick
-        </span>
-      ),
-      url:
-        "https://cdn.jsdelivr.net/gh/bfirsh/jsnes-roms@master/croom/croom.nes"
+      url: "https://cdn.jsdelivr.net/gh/bfirsh/jsnes-roms@master/croom/croom.nes"
     },
     lj65: {
       name: "LJ65",
-      description: (
-        <span>
-          <a
-            href="http://harddrop.com/wiki/LJ65"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Concentration Room
-          </a>{" "}
-          by Damian Yerrick
-        </span>
-      ),
       url: "https://cdn.jsdelivr.net/gh/bfirsh/jsnes-roms@master/lj65/lj65.nes"
     }
   },
