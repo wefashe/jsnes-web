035ec19ade32106924ea520eb845ee7de38d769a
wefashe <wefashe@qq.com>
2024-12-13 18:56:44 +0800

    终于可以通过配置文件了

---
+++ public/data.js
@@ -0,0 +1,26 @@
+var rom_list = {
+    1942: {
+        name: "1942",
+        url: "roms/classic/1942.nes"
+    },
+    mario: {
+        name: "超级玛丽",
+        url: "https://lab.ur1.fun/FCGames/roms/classic/mario.nes"
+    },
+    owlia: {
+        name: "奥薇利亚传奇",
+        url: "https://cdn.jsdelivr.net/gh/bfirsh/jsnes-roms@master/owlia.nes"
+    },
+    nomolos: {
+        name: "Nomolos: Storming the Catsle",
+        url: "https://cdn.jsdelivr.net/gh/bfirsh/jsnes-roms@master/nomolos.nes"
+    },
+    croom: {
+        name: "Concentration Room",
+        url: "https://cdn.jsdelivr.net/gh/bfirsh/jsnes-roms@master/croom/croom.nes"
+    },
+    lj65: {
+        name: "LJ65",
+        url: "https://cdn.jsdelivr.net/gh/bfirsh/jsnes-roms@master/lj65/lj65.nes"
+    }
+}
--- src/config.js
+++ src/config.js
@@ -1,32 +1,5 @@
-import React from "react";
-
 const config = {
-  ROMS: {
-    1942: {
-      name: "1942",
-      url: "roms/classic/1942.nes"
-    },
-    mario: {
-      name: "超级玛丽",
-      url: "https://lab.ur1.fun/FCGames/roms/classic/mario.nes"
-    },
-    owlia: {
-      name: "奥薇利亚传奇",
-      url: "https://cdn.jsdelivr.net/gh/bfirsh/jsnes-roms@master/owlia.nes"
-    },
-    nomolos: {
-      name: "Nomolos: Storming the Catsle",
-      url: "https://cdn.jsdelivr.net/gh/bfirsh/jsnes-roms@master/nomolos.nes"
-    },
-    croom: {
-      name: "Concentration Room",
-      url: "https://cdn.jsdelivr.net/gh/bfirsh/jsnes-roms@master/croom/croom.nes"
-    },
-    lj65: {
-      name: "LJ65",
-      url: "https://cdn.jsdelivr.net/gh/bfirsh/jsnes-roms@master/lj65/lj65.nes"
-    }
-  },
+  ROMS: window.rom_list,
   GOOGLE_ANALYTICS_CODE: process.env.REACT_APP_GOOGLE_ANALYTICS_CODE,
   SENTRY_URI: process.env.REACT_APP_SENTRY_URI
 };
